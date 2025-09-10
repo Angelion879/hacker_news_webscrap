4a6738a4b5cd1fa032b9627622c18a46e99d00d8
Angelion879 <diangelis879@outlook.com>
2025-09-10 15:37:33 -0300

    (create) main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import requests
+import scrap as s
+from bs4 import BeautifulSoup as bs
+
+def send_ntfy_message(message):
+    requests.post(f"https://{CHAN}",
+        data=f"{message}",
+        headers={
+            "Title": "Hacker News Update",
+            "Tags": "computer",
+        })
+
+
+if __name__ == '__main__':
+    site = requests.get('https://news.ycombinator.com/news')
+    HTML_SOUP = bs(site.text, 'html.parser')
+
+    try:
+        CHAN = os.environ["SECRET_CHANNEL"]
+    except KeyError:
+        from keys import channel
+        CHAN = channel
+
+    news_message = s.create_text_message(HTML_SOUP)
+
+    send_ntfy_message(news_message)
