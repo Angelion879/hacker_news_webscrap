2f420aa4a7d99d9e667fd6911d46930281c60d83
Angelion879 <diangelis879@outlook.com>
2025-09-11 12:45:27 -0300

    (add) test for the vote list function

--- tests/test_scrap.py
+++ tests/test_scrap.py
@@ -6,10 +6,8 @@ from bs4 import BeautifulSoup as bs
 sys.path.append('../')
 from src import scrap
 
-
-
-
 class TestHackerNewsScrapper(unittest.TestCase):
+    """Tests regarding the scrap.py functions"""
 
     with open('page_mock.html') as test_html:
         soap = bs(test_html, 'html.parser')
@@ -19,5 +17,13 @@ class TestHackerNewsScrapper(unittest.TestCase):
         expected = [['This is a test title', 'https://example.com'],
                     ['Another test title', 'https://example2.com'],
                     ['Guess what? Test titles!', 'https://example3.com']]
+
+        self.assertEqual(test_title_list, expected)
+
+    def test_vote_list_creation(self):
+        test_title_list = scrap.create_list_of_votes(self.soap)
+        expected = ['244',
+                    '120',
+                    '301']
 
         self.assertEqual(test_title_list, expected)
