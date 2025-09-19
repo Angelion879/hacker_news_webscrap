059b7a3f73c5a89b56b9355d8b6fe1e82bac3550
Angelion879 <diangelis879@outlook.com>
2025-09-19 12:21:40 -0300

    (fix) pylint: add functions docstrings

--- tests/test_scrap.py
+++ tests/test_scrap.py
@@ -7,12 +7,13 @@ sys.path.append('../')
 from src import scrap
 
 class TestHackerNewsScrapper(unittest.TestCase):
-    """Tests regarding the scrap.py functions"""
+    """tests regarding the scrap.py functions"""
 
     with open('page_mock.html', encoding='utf-8') as test_html:
         soap = bs(test_html, 'html.parser')
 
     def test_title_and_link_list_creation(self):
+        '''test for the create_list_of_titles_and_links feature'''
         test_title_list = scrap.create_list_of_titles_and_links(self.soap)
         expected = [['This is a test title', 'https://example.com'],
                     ['Another test title', 'https://example2.com'],
@@ -21,6 +22,7 @@ class TestHackerNewsScrapper(unittest.TestCase):
         self.assertEqual(test_title_list, expected)
 
     def test_vote_list_creation(self):
+        '''test for the create_list_of_votes feature'''
         test_vote_list = scrap.create_list_of_votes(self.soap)
         expected = ['244',
                     '120',
@@ -29,6 +31,7 @@ class TestHackerNewsScrapper(unittest.TestCase):
         self.assertEqual(test_vote_list, expected)
 
     def test_relevant_news_list_creation(self):
+        '''test for the create_relevant_news_list feature'''
         test_news_list = scrap.create_relevant_news_list(self.soap)
         expected = [['This is a test title', 'https://example.com'],
                     ['Guess what? Test titles!', 'https://example3.com']]
